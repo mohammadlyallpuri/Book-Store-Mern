6fab3c26692b9947674e86725d45d75a68b2a8de
Mohammad Bin Abdullah <141648409+mohammadlyallpuri@users.noreply.github.com>
2024-04-09 10:57:11 +0500

    Update config.js hide

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT  =5555;
 
 
 export const mongoDBURL=
-"mongodb+srv://mohammadweex21:EHHAnvWY0UxwABke@books-store-mern.9wxdcpf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
+"mongodb+srv://mohammadweex21:<password>@books-store-mern.9wxdcpf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
